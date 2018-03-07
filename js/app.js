1c290f58a85c7b9b2825f35f5b34628eef939eb1
Angela Wairimu <adelleciousm467@gmail.com>
2018-03-07 14:19:49 +0300

    add changes to js file

--- js/app.js
+++ js/app.js
@@ -1,33 +1,32 @@
-$(document).ready(function() {
-  $("#enter form").submit(function(event) {
-    event.preventDefault();
-    var display = parseInt($("input#ppnumber").val());
-    var result = answer(display);
-    $(".Push").text(result);
-  });
-});
 // business logic
-var answer = function(display) {
-  for (i = 1; i <= display; i++) {
-    // if the number is divisible by 15, write "Ping Pong"
-    if (i % 15 === 0) {
-      console.log("pingpong");
-      $(".Push").append("<li>" + "pingpong" + "</li>");
-      //return "PingPong";
+var numbers = []
+
+function game(numberInput, numbers) {
+  for (var i = 1; i <= numberInput; i++)
+    if (i % 15 == 0) {
+      numbers.push("pingpong");
+
     }
-    // if the number is divisible by 3, write "Ping"
-    else if (i % 3 === 0) {
-      $(".Push").append("<li>" + "ping" + "</>");
-      //return "Ping";
-    }
-    // otherwise, write "po"
-    else if (i % 5 === 0) {
-      $(".Push").append("<li>" + "pong" + "</>");
-      //return "Pong";
-    } else {
-      console.log("qwert");
-      $(".Push").append("<li>" + i + "</>");
-      //return(i);
-    }
+  else if (i % 3 == 0) {
+    numbers.push("ping");
+  } else if (i % 5 == 0) {
+    numbers.push("pong");
+  } else {
+    numbers.push(i);
   }
 }
+
+//user logic
+$(document).ready(function() {
+  $("#user").click(function() {
+    event.preventDefault();
+    var numberInput = parseInt($("input#number").val());
+
+    game(numberInput, numbers);
+
+    numbers.forEach(function(number) {
+      $("#resultslist").append("<li>" + number + "</li>");
+    });
+
+  });
+})
